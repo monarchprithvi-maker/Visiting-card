7a0bf2e9bd98b6324e33a23d58f41f28537c80af
monarchprithvi-maker <prithviasmyself123@gmail.com>
2025-10-05 13:21:53 +0530

    visiting card

---
+++ main.js
@@ -0,0 +1,24 @@
+mport * as THREE from '../libs/three.module.js';
+
+console.log("THREE" , THREE);
+
+document.addEventListener("DOMContentLoaded", () => {
+	const scene = new THREE.Scene();
+	const geometry = new THREE.BoxGeometry(1, 1, 1);
+	const material = new THREE.MeshBasicMaterial({color: "#0000ff"});
+	const cube = new THREE.Mesh(geometry, material);
+
+	scene.add(cube);
+	cube.position.set(0, 0, -2);
+	cube.rotation.set(0, Math.PI/4, 0, );
+
+	const camera = new THREE.PerspectiveCamera();
+	camera.position.set(1, 1, 5);
+
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setSize(500, 500);
+	renderer.render(scene, camera);
+
+	document.body.appendChild(renderer.domElement);
+
+})
